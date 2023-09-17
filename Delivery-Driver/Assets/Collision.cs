4ffe11bc001637da1e3f453b63b84fe474b88e92
John McGee <mcgee336@morrisville.edu>
2023-09-17 18:01:05 -0400

    Using OnTriggerEnter2D

--- Delivery-Driver/Assets/Collision.cs
+++ Delivery-Driver/Assets/Collision.cs
@@ -7,4 +7,9 @@ public class Collision : MonoBehaviour
      void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("Danger!");
     }
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Passed");
+    }
+
 }
