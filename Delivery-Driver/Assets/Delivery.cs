cfdc1d32afd25c6b3e479b0f984f7ba3a9a7bee4
John McGee <mcgee336@morrisville.edu>
2023-09-17 19:46:37 -0400

    If Statements & Tags

---
+++ Delivery-Driver/Assets/Delivery.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+     void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Danger!");
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package") 
+        {
+            Debug.Log("Package Picked Up");
+        }
+
+        if (other.tag == "Customer") 
+        {
+            Debug.Log("Package Delivered");
+        }
+    }
+
+}
